a7283d3150873e86db21967f115b49bcd1ccc655
joshua richards <jermain119@gmail.com>
2019-10-24 19:51:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -33,11 +33,12 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+      binding.pry
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-      binding.pry
+      
   end
 
 
